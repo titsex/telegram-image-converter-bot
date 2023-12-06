68025d06f9e5594a0b39f0457405376bf83ed895
titsex <aravan962@gmail.com>
2023-12-06 13:30:22 +0300

    update dependencies

---
+++ src/handlers/actions/-response.handler.ts
@@ -0,0 +1,22 @@
+import { generateKeyboardWithActions } from "@utils";
+import { IActionCache, IContext } from "@types";
+import { Message } from "telegraf/types";
+
+export default async function responseHandler(context: IContext, message: Message, request: IActionCache) {
+    if (!('document' in message)) return
+
+    delete request.type
+    request.fileId = message.document.file_id
+    request.fileUniqueId = message.document.file_unique_id
+    request.url = (await context.telegram.getFileLink(request.fileId)).toString()
+            
+    return await context.telegram.editMessageReplyMarkup(
+        message.chat.id,
+        message.message_id,
+        '',
+        await generateKeyboardWithActions(
+            `${message.chat.id}`,
+            request.fileUniqueId
+        ),
+    )   
+}
